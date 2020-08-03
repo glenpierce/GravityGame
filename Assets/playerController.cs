e5750bdf5726443a4c1eca9c8be3114ffd95bf72
Glen Pierce <glen.piercev@gmail.com>
2020-08-02 20:07:17 -0700

    initial commit

---
+++ Assets/playerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    public GameObject planet;
+    public float gravityConstant;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        float distance = Vector3.Distance(transform.position, planet.transform.position);
+        float distanceSquared = distance * distance;
+        float forceValue = gravityConstant / distanceSquared;
+        Vector3 forceWithDirection = (forceValue*(planet.transform.position - transform.position));
+        gameObject.GetComponent<Rigidbody>().AddForce(forceWithDirection);
+    }
+}
